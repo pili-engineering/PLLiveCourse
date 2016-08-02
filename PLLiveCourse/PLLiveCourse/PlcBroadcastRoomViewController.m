fdb49f3753cee73ad23998b0c34aaf6b7ba7a684
Moskize91 <xiangtantaozeyu@gmail.com>
2016-08-02 14:36:02 +0800

    直播：获取设备权限。

--- PLLiveCourse/PLLiveCourse/PlcBroadcastRoomViewController.m
+++ PLLiveCourse/PLLiveCourse/PlcBroadcastRoomViewController.m
@@ -29,18 +29,68 @@
     
     self.cameraStreamingSession = [self _generateCameraStreamingSession];
     
-    [self.view addSubview:({
-        UIView *preview = self.cameraStreamingSession.previewView;
-        preview.frame = self.view.bounds;
-        preview.autoresizingMask = UIViewAutoresizingFlexibleWidth |
-                                   UIViewAutoresizingFlexibleHeight;
-        preview;
-    })];
+    [self requireDevicePermissionWithComplete:^(BOOL granted) {
+        
+        if (granted) {
+            // 获取了设备权限，此时显示出 preview。
+            [self.view addSubview:({
+                UIView *preview = self.cameraStreamingSession.previewView;
+                preview.frame = self.view.bounds;
+                preview.autoresizingMask = UIViewAutoresizingFlexibleWidth |
+                UIViewAutoresizingFlexibleHeight;
+                preview;
+            })];
+        } else {
+            // 没有获取设备权限（或用户拒绝给权限）此时不可能进行推流了，直接退出房间。
+            UIAlertController *av = [UIAlertController alertControllerWithTitle:@"却少摄像头或麦克风权限"
+                                                                        message:@"即将退出房间"
+                                                                 preferredStyle:UIAlertControllerStyleAlert];
+            [av addAction:[UIAlertAction actionWithTitle:@"关闭直播"
+                                                   style:UIAlertActionStyleDestructive
+                                                 handler:^(UIAlertAction * _Nonnull action) {
+                                                     [self.navigationController popViewControllerAnimated:YES];
+                                                 }]];
+            [self presentViewController:av animated:true completion:nil];
+        }
+    }];
     
     NSURL *cloudURL = [NSURL URLWithString:@"http://pili2-demo.qiniu.com/api/stream"];
+    
+    __weak typeof(self) weakSelf = self;
     [self _generatePushURLWithCloudURL:cloudURL withComplete:^(NSURL *pushURL) {
         
+        __strong typeof(self) strongSelf = weakSelf;
+        // 当收到 pushURL 时，view controoler 可能已经提前关闭和销毁，此时不可进行推流
+        if (strongSelf) {
+            
+        }
     }];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    if (self.cameraStreamingSession.isRunning) {
+        [self.cameraStreamingSession stop];
+        NSLog(@"通知服务器已关闭推流！");
+    }
+}
+
+- (void)requireDevicePermissionWithComplete:(void (^)(BOOL granted))complete
+{
+    switch ([PLCameraStreamingSession cameraAuthorizationStatus]) {
+        case PLAuthorizationStatusAuthorized:
+            complete(YES);
+            break;
+        case PLAuthorizationStatusNotDetermined: {
+            [PLCameraStreamingSession requestCameraAccessWithCompletionHandler:^(BOOL granted) {
+                complete(granted);
+            }];
+        }
+            break;
+        default:
+            complete(NO);
+            break;
+    }
 }
 
 - (PLCameraStreamingSession *)_generateCameraStreamingSession
